33ce5f01702b95cf5cc1c66c4f2c50ad393a8176
DESKTOP-K2H8UFJ\julian <julianjlloacosta@gmail.com>
2019-10-03 16:53:36 -0500

    end desing for legalization washed

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ function addItemAllCard(text1){
 
 function legalizationParking(){
     var removeSVG='<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-    var img3=document.getElementsByClassName('imageBasic1');
+    var img3=document.getElementsByClassName('imageBasic2');
 img3.src="/img/basicImage.png";
 document.getElementById('add2').addEventListener('click',function(){
     var value2=document.getElementById('inputCount2').value;
@@ -285,6 +285,61 @@ function addItemAllCard(text2){
     list2.insertBefore(item2, list2.childNodes[0]);
     }
 }
+
+function legalizationWashed(){
+    var removeSVG='<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+    var img4=document.getElementsByClassName('imageBasic3');
+    img4.src="/img/basicImage.png";
+    document.getElementById('add3').addEventListener('click',function(){
+     var value3=document.getElementById('inputCount3').value;
+        if(value3){
+         addItemAllCard(value3);
+         var value3=document.getElementById('inputCount3').value='';
+        } 
+    });
+function removeItem3(){
+    var item3=this.parentNode.parentNode;
+    var parent3=item3.parentNode;
+    parent3.removeChild(item3);
+}
+function addItemAllCard(text3){
+    var list3=document.getElementById("allCard3");
+
+    var item3=document.createElement('li');
+
+    var card3=document.createElement('div');
+    card3.classList.add('contentCard3');
+    
+
+    var report3=document.createElement('img');
+    report3.classList.add('imageBasic3');
+    report3.innerHTML=img4;
+
+    var valueTolls3=document.createElement('span');
+    valueTolls3.classList.add('valueBasic3');
+    valueTolls3.innerText=text3;
+
+    var buttons3=document.createElement('div');
+    buttons3.classList.add('buttons3');
+
+    var remove3=document.createElement('button');
+    remove3.classList.add('remove3');
+    remove3.innerHTML=removeSVG;
+
+    remove3.addEventListener('click', removeItem3);
+
+    buttons3.appendChild(remove3);
+    item3.appendChild(buttons3);
+    card3.appendChild(report3);
+    card3.appendChild(valueTolls3);
+    item3.appendChild(card3);
+
+
+    list3.insertBefore(item3, list3.childNodes[0]);
+    }
+}
+
+
 
 function getDriverTruck() {
     let lista = document.getElementById("listaCamiones");

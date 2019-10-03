7c9ddeceec747510901ab9df0f407c8e161711e1
DESKTOP-K2H8UFJ\julian <julianjlloacosta@gmail.com>
2019-10-03 16:20:57 -0500

    completed section legalization parking design

--- js/main.js
+++ js/main.js
@@ -145,7 +145,7 @@ function addItemAllCard(text){
 
     var item=document.createElement('li');
 
-    var card=document.createElement('div')
+    var card=document.createElement('div');
     card.classList.add('contentCard');
     
 
@@ -190,7 +190,7 @@ document.getElementById('add1').addEventListener('click',function(){
     } 
 
 });
-function removeItem(){
+function removeItem1(){
     var item1=this.parentNode.parentNode;
     var parent1=item1.parentNode;
     parent1.removeChild(item1);
@@ -200,7 +200,7 @@ function addItemAllCard(text1){
 
     var item1=document.createElement('li');
 
-    var card1=document.createElement('div')
+    var card1=document.createElement('div');
     card1.classList.add('contentCard1');
     
 
@@ -219,7 +219,7 @@ function addItemAllCard(text1){
     remove1.classList.add('remove1');
     remove1.innerHTML=removeSVG;
 
-    remove1.addEventListener('click', removeItem);
+    remove1.addEventListener('click', removeItem1);
 
     buttons1.appendChild(remove1);
     item1.appendChild(buttons1);
@@ -232,6 +232,59 @@ function addItemAllCard(text1){
     }
 }
 
+function legalizationParking(){
+    var removeSVG='<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+    var img3=document.getElementsByClassName('imageBasic1');
+img3.src="/img/basicImage.png";
+document.getElementById('add2').addEventListener('click',function(){
+    var value2=document.getElementById('inputCount2').value;
+    if(value2){
+        addItemAllCard(value2);
+        var value2=document.getElementById('inputCount2').value='';
+    } 
+
+});
+function removeItem2(){
+    var item2=this.parentNode.parentNode;
+    var parent2=item2.parentNode;
+    parent2.removeChild(item2);
+}
+function addItemAllCard(text2){
+    var list2=document.getElementById("allCard2");
+
+    var item2=document.createElement('li');
+
+    var card2=document.createElement('div');
+    card2.classList.add('contentCard2');
+    
+
+    var report2=document.createElement('img');
+    report2.classList.add('imageBasic2');
+    report2.innerHTML=img3;
+
+    var valueTolls2=document.createElement('span');
+    valueTolls2.classList.add('valueBasic2');
+    valueTolls2.innerText=text2;
+
+    var buttons2=document.createElement('div');
+    buttons2.classList.add('buttons2');
+
+    var remove2=document.createElement('button');
+    remove2.classList.add('remove2');
+    remove2.innerHTML=removeSVG;
+
+    remove2.addEventListener('click', removeItem2);
+
+    buttons2.appendChild(remove2);
+    item2.appendChild(buttons2);
+    card2.appendChild(report2);
+    card2.appendChild(valueTolls2);
+    item2.appendChild(card2);
+
+
+    list2.insertBefore(item2, list2.childNodes[0]);
+    }
+}
 
 function getDriverTruck() {
     let lista = document.getElementById("listaCamiones");

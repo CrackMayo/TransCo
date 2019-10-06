eafe619047f92cc0a560eabcacddaa47abaa170f
Victor Mauricio Restrepo Ossa <victor.restrepoo@upb.edu.co>
2019-10-05 21:31:48 -0500

    Basic upload settelement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var spendings = [];
+var currentPlate;
 
 
 function userDataLogin(userId) {
@@ -432,6 +433,7 @@ function legalizationOthers() {
     var removeSVG = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
     var img5 = document.getElementsByClassName('imageBasic4');
     img5.src = "/img/basicImage.png";
+    console.log("other Plate" + currentPlate);
     document.getElementById('add4').addEventListener('click', function () {
         var value4 = document.getElementById('inputCount4').value;
         var imgField = document.getElementById('file-tolls4').files[0];
@@ -616,8 +618,13 @@ function uploadImageTruck(imagen, placa) {
     });
 
 }
-function viewTruck() {
+function viewTruck(Plate) {
+
+    currentPlate = Plate;
+
     changePage('view-truck', 'section-initial-page');
+
+    console.log("current Plate" + currentPlate);
 }
 function changeLegalization() {
     changePage('create-travel', 'view-truck');
@@ -839,6 +846,8 @@ function addSpending(idInput, typeSpend, imgSpend) {
 
 
 function uploadSettlement() {
+    let numberSettlemet;
+    let numberSpending;
 
     console.log(spendings);
 
@@ -856,9 +865,36 @@ function uploadSettlement() {
 
     }
 
-    console.log(spendingH);
+    db.collection('accounts').doc(idUsuario).collection("camiones").doc(currentPlate).collection("liquidaciones").get().then(snapshot => {
+        numberSettlemet = snapshot.size + 1;
 
-    db.collection('accounts').doc(idUsuario).collection("camiones").doc("SRM632").collection("liquidaciones").doc("1").set(spendingH).then(function () {
+        db.collection('accounts').doc(idUsuario).collection("camiones").doc(currentPlate).collection("liquidaciones").doc(numberSettlemet.toString()).set(spendingH).then(function () {
+
+            for (let i = 1; i <= spendings.length - 1; i++) {
+
+                var spendingN = {
+                    valor: spendings[i].value,
+                    // imagen: spendings[i].image,
+                    categoria: spendings[i].type
+
+                }
+
+                numberSpending = i;
+
+                db.collection('accounts').doc(idUsuario).collection("camiones").doc(currentPlate).collection("liquidaciones").doc(numberSettlemet.toString()).collection("gastos").doc(i.toString()).set(spendingN).then(function () {
+
+
+                }).catch(function (error) {
+                    console.error("Error: ", error);
+                });
+            }
+
+
+
+        }).catch(function (error) {
+            console.error("Error: ", error);
+        });
+
 
 
     }).catch(function (error) {
@@ -866,19 +902,14 @@ function uploadSettlement() {
     });
 
 
-    // var spendingN = {
-    //     valor: spendings[0].value,
-    //     imagen: spendings[0].image,
-    //     categoria: spendings[0].type
-
-    // }
-
-    // db.collection('accounts').doc(idUsuario).collection("camiones").doc("SRM632").collection("liquidaciones").doc("1").collection("gastos").doc("1").set(spendingN).then(function () {
 
 
-    // }).catch(function (error) {
-    //     console.error("Error: ", error);
-    // });
+
+
+
+
+
+
 
 
 
